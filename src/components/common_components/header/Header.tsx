c66a5855ef9610009690d1e70340146402c5ea3e
Nazar Yermolenko <nazaron1995@gmail.com>
2022-05-23 23:34:19 +0300

    Added simple style for header

--- src/components/common_components/header/Header.tsx
+++ src/components/common_components/header/Header.tsx
@@ -2,19 +2,17 @@ import { Link } from "react-router-dom"
 
 export function Header() {
     return <header className="header" id="header">
-        <div className="container">
+        <div className="container header_container">
             <div className="logo">
                 <Link to="/">
                     <h1>LOGO</h1>
                 </Link>
             </div>
-            <div className="menu">
-                <ul>
-                    <li className="menu_item"><Link to="/sign_in">Sign In/Register</Link></li>
-                    <li className="menu_item">Cart</li>
-                    <li className="menu_item"><Link to="/about">About</Link></li>
-                </ul>
-            </div>
+            <ul className="menu">
+                <li className="menu_item"><Link to="/sign_in">Sign In/Register</Link></li>
+                <li className="menu_item">Cart</li>
+                <li className="menu_item"><Link to="/about">About</Link></li>
+            </ul>
         </div>
     </header>
 }
