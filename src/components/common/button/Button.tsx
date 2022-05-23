d06e5d1a0174fb2cf87202773ebbc851ce4586c2
Nazar Yermolenko <nazaron1995@gmail.com>
2022-05-23 19:21:06 +0300

    Minor fixes

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,5 +1,3 @@
-import { IShopItem } from "../../home_page/shop/shopItem/IShopItem";
-
 export function Button(props: { onClick, children }) {
     return <button type={"button"} onClick={props.onClick} className="button">
         {props.children}
