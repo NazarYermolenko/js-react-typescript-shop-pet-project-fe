c075930ff5556042dc774fd5d131840c161e2480
Nazar Yermolenko <nazaron1995@gmail.com>
2022-05-23 23:21:00 +0300

    Created index.css

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import HomePage from './components/home_page/HomePage';
 import { NotFoundPage } from './components/not_found_page/NotFoundPage';
 import { SignInRegisterPage } from './components/sing_in_register_page/SignInRegisterPage';
 import "./index.css"
+import "./reset.css"
 
 function App() {
-    return (
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='*' element={<NotFoundPage />} />
-        <Route path="/sign_in" element={<SignInRegisterPage />} />
-        <Route path='/about' element={<AboutPage />} />
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='*' element={<NotFoundPage />} />
+      <Route path="/sign_in" element={<SignInRegisterPage />} />
+      <Route path='/about' element={<AboutPage />} />
+    </Routes>
+  );
+}
 
 export default App;
