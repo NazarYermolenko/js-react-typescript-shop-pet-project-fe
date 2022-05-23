1dc1d715bd50dcbac7acb3b7c500834b58f8e1a1
Nazar Yermolenko <nazaron1995@gmail.com>
2022-05-23 23:12:32 +0300

    reset.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { AboutPage } from './components/aboutPage/AboutPage';
 import HomePage from './components/home_page/HomePage';
 import { NotFoundPage } from './components/not_found_page/NotFoundPage';
 import { SignInRegisterPage } from './components/sing_in_register_page/SignInRegisterPage';
+import "./index.css"
 
 function App() {
-  return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='*' element={<NotFoundPage />} />
-      <Route path="/sign_in" element={<SignInRegisterPage />}/>
-      <Route path='/about' element={<AboutPage />} />
-    </Routes>
-  );
-}
+    return (
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<NotFoundPage />} />
+        <Route path="/sign_in" element={<SignInRegisterPage />} />
+        <Route path='/about' element={<AboutPage />} />
+      </Routes>
+    );
+  }
 
 export default App;
